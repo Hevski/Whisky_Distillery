d08e823f6e8aedc7f365033fa98a2ed1d283392c
Hevski <heathercmalloch@gmail.com>
2019-04-04 07:42:12 +0100

    added test

--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,7 +1,9 @@
 package com.codeclan.example.WhiskyTracker;
 
 import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
 import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,9 @@ public class WhiskyTrackerApplicationTests {
 	@Autowired
 	DistilleryRepository distilleryRepository;
 
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
 	@Test
 	public void contextLoads() {
 	}
@@ -35,4 +40,18 @@ public class WhiskyTrackerApplicationTests {
 		List<Distillery> found = distilleryRepository.findAllDistilleriesByRegion("Speyside");
 		assertEquals(2, found.size());
 	}
+
+	@Test
+	public void findAllWhiskiesForAGivenYear(){
+		List<Whisky> found = whiskyRepository.findAllWhiskyByYear(1991);
+		assertEquals(1, found.size());
+	}
+
+//	@Test
+//	public void findAllWhiskyByDistillery(){
+//		List<Whisky> found = whiskyRepository.findAllWhiskyByDistillery("Macallan");
+//		assertEquals(1, found.size());
+//	}
+
+
 }
